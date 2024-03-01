835f950239231e51c63e5afe81fa3e9a8903ddd9
이정훈 <eu4ng97@gmail.com>
2024-03-01 21:39:51 +0900

    feat: GEGameplayAbilityBase 클래스 작성
    기본 인스턴싱 정책 및 Blocking 태그 설정

---
+++ GASExtension/Source/GASExtension/Private/GEGameplayAbilityBase.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GEGameplayAbilityBase.h"
+
+#include "GEHealthAttributeSet.h"
+
+UGEGameplayAbilityBase::UGEGameplayAbilityBase()
+{
+    // 기본 인스턴싱 정책 설정
+    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+    // 기본 Blocking 태그 설정
+    ActivationBlockedTags.AddTag(GEGameplayTags::DeadTag);
+}
+
+void UGEGameplayAbilityBase::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+    Super::OnAvatarSet(ActorInfo, Spec);
+
+    if(bActivateAbilityOnGranted)
+    {
+        ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle, false);
+    }
+}
---
+++ GASExtension/Source/GASExtension/Public/GEGameplayAbilityBase.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GEGameplayAbilityBase.generated.h"
+
+/**
+ *
+ */
+UCLASS(Blueprintable, BlueprintType)
+class GASEXTENSION_API UGEGameplayAbilityBase : public UGameplayAbility
+{
+    GENERATED_BODY()
+
+public:
+    UGEGameplayAbilityBase();
+
+    /* UGameplayAbility */
+
+    virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+    // true 설정 시 Ability가 부여되는 즉시 해당 Ability를 발동합니다.
+    // HitReact처럼 수동적인 Ability에서 사용합니다.
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Config")
+    bool bActivateAbilityOnGranted = false;
+};
