728def39f531734cdcf7a7494c2bbf78182c3d6f
이정훈 <eu4ng97@gmail.com>
2024-03-02 00:33:04 +0900

    feat: GEGA_ApplyEffectsToSelf 클래스 작성
    Ability를 발동한 주체에게 GameplayEffects를 적용하는 Ability입니다.
    AttributeSet 초기화, 아이템 효과 적용 등을 위해 사용할 수 있습니다.
    사용 방법은 적용할 대상에게 GiveAbility로 전달하면 됩니다.

---
+++ GASExtension/Source/GASExtension/Private/Abilities/GEGA_ApplyEffectsToSelf.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Abilities/GEGA_ApplyEffectsToSelf.h"
+
+#include "AbilitySystemComponent.h"
+
+void UGEGA_ApplyEffectsToSelf::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                               const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                               const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    // AbilitySystemComponent 획득
+    UAbilitySystemComponent* AbilitySystem = ActorInfo->AbilitySystemComponent.Get();
+
+    for (const auto& EffectClass : EffectsToApply)
+    {
+        // 유효성 검사
+        if(EffectClass == nullptr) continue;
+
+        // Ability 발동 주체에 GameplayEffect 적용
+        FGameplayEffectSpecHandle GameplayEffectSpecHandle = AbilitySystem->MakeOutgoingSpec(EffectClass, 0, MakeEffectContext(Handle, ActorInfo));
+        if(GameplayEffectSpecHandle.IsValid())
+        {
+            AbilitySystem->ApplyGameplayEffectSpecToSelf(*GameplayEffectSpecHandle.Data.Get());
+        }
+    }
+
+    // Ability 종료
+    EndAbility(Handle, ActorInfo, ActivationInfo, false, false);
+}
---
+++ GASExtension/Source/GASExtension/Public/Abilities/GEGA_ApplyEffectsToSelf.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GEGA_AutoActivatable.h"
+#include "GEGA_ApplyEffectsToSelf.generated.h"
+
+/**
+ * Ability를 발동한 주체에게 GameplayEffects를 적용하는 Ability입니다.
+ * AttributeSet 초기화, 아이템 효과 적용 등을 위해 사용할 수 있습니다.
+ * 사용 방법은 적용할 대상에게 GiveAbility로 전달하면 됩니다.
+ */
+UCLASS()
+class GASEXTENSION_API UGEGA_ApplyEffectsToSelf : public UGEGA_AutoActivatable
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, Category = "Config", meta = (AllowPrivateAccess = true))
+    TArray<TSubclassOf<UGameplayEffect>> EffectsToApply;
+
+public:
+    /* UGameplayAbility */
+
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
