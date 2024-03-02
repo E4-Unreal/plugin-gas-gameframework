c80dbf1b977f98f04acaa93614f2d8886d4f4bf4
이정훈 <eu4ng97@gmail.com>
2024-03-02 21:15:33 +0900

    feat: GameplayEffectApplier 클래스 작성
    대상 액터에게 GameplayEffects를 적용할 수 있도록 도와주는 간단한 액터 컴포넌트입니다.

---
+++ GASExtension/Source/GASExtension/Private/Components/GameplayEffectApplier.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/GameplayEffectApplier.h"
+
+#include "FunctionLibraries/GEFunctionLibrary.h"
+
+void UGameplayEffectApplier::ApplyEffectsToTarget(AActor* Target)
+{
+    UGEFunctionLibrary::ApplyGameplayEffectsToTarget(EffectsToApply, GetOwner()->GetInstigator(), Target);
+}
