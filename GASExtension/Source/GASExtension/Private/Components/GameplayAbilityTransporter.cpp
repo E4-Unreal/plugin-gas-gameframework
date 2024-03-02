f043c6df0673b378abbb42d76680fd6ed9b8bc5d
이정훈 <eu4ng97@gmail.com>
2024-03-03 06:46:58 +0900

    feat: GameplayEffectApplier 클래스 작성
    대상 액터에게 GameplayAbility를 부여할 수 있도록 도와주는 간단한 액터 컴포넌트입니다.
    무기를 장착하거나 아이템을 사용하였을 때 GameplayAbility를 부여하는 경우 사용할 수 있습니다.

---
+++ GASExtension/Source/GASExtension/Private/Components/GameplayAbilityTransporter.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/GameplayAbilityTransporter.h"
+
+#include "FunctionLibraries/GEFunctionLibrary.h"
+
+
+void UGameplayAbilityTransporter::GiveAbilitiesToTarget(AActor* Target)
+{
+    UGEFunctionLibrary::GiveAbilitiesToTarget(AbilitiesToGive, Target);
+}
