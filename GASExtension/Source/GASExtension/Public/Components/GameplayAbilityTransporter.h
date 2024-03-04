b390bcc43d6e7389ee9ac73b02e45d2982b2335a
이정훈 <eu4ng97@gmail.com>
2024-03-04 20:56:19 +0900

    feat: EffectArea 클래스 작성
    모닥불처럼 주기적으로 오버랩된 액터들에게 GameplayEffect를 적용하기 위한 액터 컴포넌트입니다.

--- GASExtension/Source/GASExtension/Public/Components/GameplayAbilityTransporter.h
+++ GASExtension/Source/GASExtension/Public/Components/GameplayAbilityTransporter.h
@@ -11,7 +11,7 @@ class UGameplayAbility;
  * 대상 액터에게 GameplayAbility를 부여할 수 있도록 도와주는 간단한 액터 컴포넌트입니다.
  * 무기를 장착하거나 아이템을 사용하였을 때 GameplayAbility를 부여하는 경우 사용할 수 있습니다.
  */
-UCLASS(meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup=(GASExtesion), meta=(BlueprintSpawnableComponent))
 class GASEXTENSION_API UGameplayAbilityTransporter : public UActorComponent
 {
     GENERATED_BODY()
--- GASExtension/Source/GASExtension/Public/Components/GameplayEffectApplier.h
+++ GASExtension/Source/GASExtension/Public/Components/GameplayEffectApplier.h
@@ -12,7 +12,7 @@ class UGameplayEffect;
  * 대상 액터에게 GameplayEffects를 적용할 수 있도록 도와주는 간단한 액터 컴포넌트입니다.
  * 총알이나 불처럼 충돌 이벤트에 ApplyEffectsToTarget 메서드를 바인딩하여 사용하면 됩니다.
  */
-UCLASS(meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup=(GASExtesion), meta=(BlueprintSpawnableComponent))
 class GASEXTENSION_API UGameplayEffectApplier : public UActorComponent
 {
     GENERATED_BODY()
