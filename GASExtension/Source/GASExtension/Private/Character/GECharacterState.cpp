711640ee07b8868a3f1822fdfe052804f8b29751
이정훈 <eu4ng97@gmail.com>
2024-03-12 10:53:26 +0900

    feat: 상태 머신 클래스 GEStateMachine, GEState 작성

---
+++ GASExtension/Source/GASExtension/Private/Character/GECharacterState.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/GECharacterState.h"
+
+#include "AbilitySystemComponent.h"
+#include "Character/GEStateMachine.h"
+#include "GameFramework/Character.h"
+
+void UGECharacterState::OnInit_Implementation()
+{
+    Super::OnInit_Implementation();
+
+    Character = Cast<ACharacter>(GetStateMachine()->GetAbilitySystem()->GetOwnerActor());
+}
