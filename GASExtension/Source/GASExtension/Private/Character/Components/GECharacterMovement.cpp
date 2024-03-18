cfd4fba181b807cacdc75b9e8a0344e9cc5bdce9
이정훈 <eu4ng97@gmail.com>
2024-03-18 23:01:31 +0900

    refactor: 캐릭터 무브먼트 컴포넌트 병합

--- GASExtension/Source/GASExtension/Private/Character/Components/GECharacterMovement.cpp
+++ GASExtension/Source/GASExtension/Private/Character/Components/GECharacterMovement.cpp
@@ -1,12 +1,25 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Character/Components/GECharacterMovement.h"
 
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "GEGameplayTags.h"
 #include "GameFramework/Character.h"
 
 UGECharacterMovement::UGECharacterMovement()
 {
+    // 초기화
+    bWantsInitializeComponent = true;
+
+    // 캐릭터 무브먼트 설정
+    NavAgentProps.bCanCrouch = true;
+    bCanWalkOffLedgesWhenCrouching = true;
+
+    // 태그 기본값 설정
+    FallingTag = GEGameplayTags::State::FallingTag;
+    CrouchingTag = GEGameplayTags::State::CrouchingTag;
+
     // 일반 설정
     GravityScale = 1.5f;
     MaxAcceleration = 3000.f;
@@ -24,6 +37,49 @@ UGECharacterMovement::UGECharacterMovement()
     // 점프 및 낙하 설정
     JumpZVelocity = 600.f;
     AirControl = 0.35f;
+}
+
+void UGECharacterMovement::InitializeComponent()
+{
+    Super::InitializeComponent();
+
+    OwnerAbilitySystem = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(PawnOwner);
+}
+
+void UGECharacterMovement::SetMovementMode(EMovementMode NewMovementMode, uint8 NewCustomMode)
+{
+    if(MovementMode != NewMovementMode && OwnerAbilitySystem.IsValid())
+    {
+        // Falling 게임플레이 태그 부착 혹은 제거
+        if(NewMovementMode == MOVE_Falling)
+            OwnerAbilitySystem->AddLooseGameplayTag(FallingTag);
+        else if(MovementMode == MOVE_Falling)
+            OwnerAbilitySystem->RemoveLooseGameplayTag(FallingTag);
+    }
+
+    Super::SetMovementMode(NewMovementMode, NewCustomMode);
+}
+
+void UGECharacterMovement::Crouch(bool bClientSimulation)
+{
+    // Crouch 게임플레이 태그 부착
+    if(OwnerAbilitySystem.IsValid())
+    {
+        OwnerAbilitySystem->AddLooseGameplayTag(CrouchingTag);
+    }
+
+    Super::Crouch(bClientSimulation);
+}
+
+void UGECharacterMovement::UnCrouch(bool bClientSimulation)
+{
+    // Crouch 게임플레이 태그 제거
+    if(OwnerAbilitySystem.IsValid())
+    {
+        OwnerAbilitySystem->RemoveLooseGameplayTag(CrouchingTag);
+    }
+
+    Super::UnCrouch(bClientSimulation);
 }
 
 bool UGECharacterMovement::CanAttemptJump() const
--- GASExtension/Source/GASExtension/Public/Character/Components/GECharacterMovement.h
+++ GASExtension/Source/GASExtension/Public/Character/Components/GECharacterMovement.h
@@ -3,24 +3,50 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GECharacterMovementBase.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GECharacterMovement.generated.h"
 
+class UAbilitySystemComponent;
+
 /*
- * 앉은 상태에서도 점프가 가능합니다.
- * 또한, 일반적으로 사용되는 설정이 적용되어 있습니다.
+ * 캐릭터 무브먼트에 따라 상태 태그가 자동으로 부착됩니다.
  */
 UCLASS()
-class GASEXTENSION_API UGECharacterMovement : public UGECharacterMovementBase
+class GASEXTENSION_API UGECharacterMovement : public UCharacterMovementComponent
 {
     GENERATED_BODY()
+
+protected:
+    TWeakObjectPtr<UAbilitySystemComponent> OwnerAbilitySystem;
+
+    UPROPERTY(EditAnywhere, Category = "Config")
+    FGameplayTag FallingTag;
+
+    UPROPERTY(EditAnywhere, Category = "Config")
+    FGameplayTag CrouchingTag;
 
 public:
     UGECharacterMovement();
 
+    /* ActorComponent */
+
+    virtual void InitializeComponent() override;
+
     /* CharacterMovementComponent */
 
+    // 점프 혹은 낙하 상태 태그 부착 혹은 제거
+    virtual void SetMovementMode(EMovementMode NewMovementMode, uint8 NewCustomMode) override;
+
+    // 앉기 상태 태그 부착
+    virtual void Crouch(bool bClientSimulation) override;
+
+    // 앉기 상태 태그 제거
+    virtual void UnCrouch(bool bClientSimulation) override;
+
+    // 앉은 상태에서 점프 가능
     virtual bool CanAttemptJump() const override;
 
+    // 앉은 상태에서 점프를 하는 경우 UnCrouch를 먼저 호출
     virtual bool DoJump(bool bReplayingMoves) override;
 };
