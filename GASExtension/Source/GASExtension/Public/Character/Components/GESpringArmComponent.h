8db6d231d0ebc8626440a9825efb560f363f2ed3
이정훈 <eu4ng97@gmail.com>
2024-03-20 08:20:14 +0900

    feat: 카메라 랙 기능을 Z축에만 적용할 수 있는 옵션이 추가된 GESpringArmComponent 클래스 작성

---
+++ GASExtension/Source/GASExtension/Public/Character/Components/GESpringArmComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GESpringArmComponent.generated.h"
+
+/**
+ * 카메라 랙 기능이 기본 활성화되어 있으며 Z축에만 적용될 수 있는 설정이 추가된 스프링 암 컴포넌트입니다.
+ * Crouch, UnCrouch 호출 시 부드러운 카메라 이동을 위해 카메라 랙 기능이 필요하지만 좌우로 달릴 때는 불필요하다고 느껴질 때 사용하면 유용합니다.
+ */
+UCLASS(meta=(BlueprintSpawnableComponent))
+class GASEXTENSION_API UGESpringArmComponent : public USpringArmComponent
+{
+    GENERATED_BODY()
+
+    // true 설정 시 카메라 랙 기능이 Z축에만 적용됩니다.
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config", meta = (AllowPrivateAccess = true))
+    bool bLimitCameraLagToZ = true;
+
+public:
+    UGESpringArmComponent();
+
+protected:
+    virtual void UpdateDesiredArmLocation(bool bDoTrace, bool bDoLocationLag, bool bDoRotationLag, float DeltaTime) override;
+};
