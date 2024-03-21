0cff514248c526096facde6541bcdb2bd1699598
이정훈 <eu4ng97@gmail.com>
2024-03-21 16:39:33 +0900

    fix: GEHitScanFireArm 클래스의 일부 멤버 변수 접근 지정자 수정
    Private > Protected로 수정
    - EffectsToApply
    - MaxDistance
    - bEnableDebugLine

--- GASExtension/Source/GASExtension/Public/Weapon/FireArm/GEHitScanFireArm.h
+++ GASExtension/Source/GASExtension/Public/Weapon/FireArm/GEHitScanFireArm.h
@@ -14,23 +14,25 @@ class GASEXTENSION_API AGEHitScanFireArm : public AGEFireArm
 {
     GENERATED_BODY()
 
+protected:
     // 피격 대상에게 적용할 GE 목록입니다.
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config|HitScan", meta = (AllowPrivateAccess = true))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config|HitScan")
     TArray<TSubclassOf<UGameplayEffect>> EffectsToApply;
 
     // 라인 트레이스의 최대 거리입니다.
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config|HitScan", meta = (AllowPrivateAccess = true))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config|HitScan")
     float MaxDistance = 100000.f;
 
-    // 피격 효과
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config|HitScan", meta = (AllowPrivateAccess = true))
-    UGEHitEffectDefinition* HitEffect;
-
 #if WITH_EDITORONLY_DATA
-  // 디버그를 위한 라인 트레이스 시각화 기능 활성화
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config|HitScan", meta = (AllowPrivateAccess = true))
+    // 디버그를 위한 라인 트레이스 시각화 기능 활성화
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Config|HitScan")
     bool bEnableDebugLine;
 #endif
+
+private:
+    // 피격 효과
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Config|HitScan", meta = (AllowPrivateAccess = true))
+    UGEHitEffectDefinition* HitEffect;
 
 protected:
     virtual void OnFire_Implementation() override;
