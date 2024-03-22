fa89280b836fa78d1cc462f2d80fc43282473e1d
이정훈 <eu4ng97@gmail.com>
2024-03-22 16:15:33 +0900

    feat: 기본 FPS 애님 인스턴스 및 애니메이션 블루프린트 작성

--- GASExtension/Source/GASExtension/Private/Character/Animation/GEAnimInstance.cpp
+++ GASExtension/Source/GASExtension/Private/Character/Animation/GEAnimInstance.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/GEAnimInstance.h"
+#include "Character/Animation/GEAnimInstance.h"
 
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemGlobals.h"
---
+++ GASExtension/Source/GASExtension/Private/Character/Animation/GEFirstPersonAnimInstance.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Character/Animation/GEFirstPersonAnimInstance.h"
+
+#include "GEGameplayTags.h"
+#include "Logging.h"
+
+UGEFirstPersonAnimInstance::UGEFirstPersonAnimInstance()
+{
+    TagsToRegister.AddTagFast(GEGameplayTags::State::Moving);
+    TagsToRegister.AddTagFast(GEGameplayTags::State::ADS);
+}
+
+void UGEFirstPersonAnimInstance::OnGameplayEffectTagCountChanged_Event(const FGameplayTag Tag, int32 NewCount)
+{
+    Super::OnGameplayEffectTagCountChanged_Event(Tag, NewCount);
+
+    const bool Value = NewCount > 0;
+
+    using namespace GEGameplayTags::State;
+
+    if(Tag.MatchesTag(Moving))
+        bMoving = Value;
+
+    if(Tag.MatchesTag(Running))
+        bRunning = Value;
+
+    if(Tag.MatchesTag(Sprinting))
+        bSprinting = Value;
+
+    if(Tag.MatchesTag(ADS))
+        bADS = Value;
+
+    if(!Tag.MatchesAny(TagsToRegister))
+    {
+#if WITH_EDITOR
+        LOG_CALLINFO_COMMENT(LogGASExtension, Warning, TEXT("태그 이벤트가 등록되었지만 사용되지 않고 있습니다: %s"), *Tag.GetTagName().ToString())
+#endif
+    }
+}
