e66dcdebabb4d42a1032a60b063af34784d71eb1
이정훈 <eu4ng97@gmail.com>
2024-03-28 14:20:14 +0900

    refactor: 상태 커스텀 섹션 생성 및 코드 리팩토링 DetailCustomizations 파일을 참고하여 코드 리팩토링

--- Source/GASGameFramework/Private/GASGameFramework.cpp
+++ Source/GASGameFramework/Private/GASGameFramework.cpp
@@ -14,30 +14,55 @@ void FGASGameFrameworkModule::StartupModule()
     // TargetData를 사용하기 위한 초기화
     UAbilitySystemGlobals::Get().InitGlobalData();
 
-    /* 디테일 패널에 커스텀 섹션 추가 */
 #if WITH_EDITOR
-#define LOCTEXT_NAMESPACE "Custom Detail"
-    static const FName PropertyEditor("PropertyEditor");
-    FPropertyEditorModule& PropertyModule = FModuleManager::GetModuleChecked<FPropertyEditorModule>(PropertyEditor);
-
-    // Actor 클래스에 커스텀 섹션 생성 및 카테고리 추가
-    const TSharedRef<FPropertySection> ActorConfigSection = PropertyModule.FindOrCreateSection("Actor", "Config", LOCTEXT("Config", "설정"));
-    ActorConfigSection->AddCategory("Config");
-    ActorConfigSection->AddCategory("State");
-
-    // ActorComponent 클래스에 커스텀 섹션 생성 및 카테고리 추가
-    const TSharedRef<FPropertySection> ActorComponentConfigSection = PropertyModule.FindOrCreateSection("ActorComponent", "Config", LOCTEXT("Config", "설정"));
-    ActorComponentConfigSection->AddCategory("Config");
-    ActorComponentConfigSection->AddCategory("State");
-
-#undef LOCTEXT_NAMESPACE
+    // 디테일 패널에 커스텀 섹션 추가
+    RegisterSectionMappings();
 #endif
 }
 
 void FGASGameFrameworkModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+
 }
 
 #undef LOCTEXT_NAMESPACE
+
+#if WITH_EDITOR
+#define LOCTEXT_NAMESPACE "Custom Detail"
+void FGASGameFrameworkModule::RegisterSectionMappings()
+{
+    static const FName PropertyEditor("PropertyEditor");
+    FPropertyEditorModule& PropertyModule = FModuleManager::GetModuleChecked<FPropertyEditorModule>(PropertyEditor);
+
+    // Actor
+    {
+        {
+            // Config, 설정
+            const TSharedRef<FPropertySection> ActorConfigSection = PropertyModule.FindOrCreateSection("Actor", "Config", LOCTEXT("Config", "설정"));
+            ActorConfigSection->AddCategory("Config");
+        }
+
+        {
+            // State, 상태
+            const TSharedRef<FPropertySection> ActorConfigSection = PropertyModule.FindOrCreateSection("Actor", "State", LOCTEXT("State", "상태"));
+            ActorConfigSection->AddCategory("State");
+        }
+    }
+
+    // ActorComponent
+    {
+        {
+            // Config, 설정
+            const TSharedRef<FPropertySection> ActorComponentConfigSection = PropertyModule.FindOrCreateSection("ActorComponent", "Config", LOCTEXT("Config", "설정"));
+            ActorComponentConfigSection->AddCategory("Config");
+        }
+
+        {
+            // State, 상태
+            const TSharedRef<FPropertySection> ActorComponentConfigSection = PropertyModule.FindOrCreateSection("ActorComponent", "State", LOCTEXT("State", "상태"));
+            ActorComponentConfigSection->AddCategory("State");
+        }
+    }
+}
+#undef LOCTEXT_NAMESPACE
+#endif
--- Source/GASGameFramework/Public/GASGameFramework.h
+++ Source/GASGameFramework/Public/GASGameFramework.h
@@ -12,4 +12,10 @@ public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+private:
+#if WITH_EDITOR
+    // 디테일 패널에 커스텀 섹션 추가
+    void RegisterSectionMappings();
+#endif
 };
