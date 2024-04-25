dc2d2742babb6bc1e1e089e821b084e78a212683
이정훈 <eu4ng97@gmail.com>
2024-04-25 18:24:59 +0900

    chore: InventorySystem 모듈에 ItemSystem 모듈 의존성 추가

--- Source/GGFInventorySystem/GGFInventorySystem.Build.cs
+++ Source/GGFInventorySystem/GGFInventorySystem.Build.cs
@@ -19,7 +19,8 @@ public class GGFInventorySystem : ModuleRules
                 "CoreUObject",
                 "Engine",
                 "Slate",
-                "SlateCore"
+                "SlateCore",
+                "GGFItemSystem"
             }
         );
     }
