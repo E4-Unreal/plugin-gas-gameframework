85f246bef74c6a0549740b190db81d84ae74829f
이정훈 <eu4ng97@gmail.com>
2024-04-25 18:22:01 +0900

    chore: GGFInventorySystem 모듈 템플릿 추가

---
+++ Source/GGFInventorySystem/Private/GGFInventorySystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "GGFInventorySystem.h"
+
+#define LOCTEXT_NAMESPACE "FGGFInventorySystemModule"
+
+void FGGFInventorySystemModule::StartupModule()
+{
+    
+}
+
+void FGGFInventorySystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGGFInventorySystemModule, GGFInventorySystem)
