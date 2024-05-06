efcb9f8cdb35cff7e1d9f20029df9b7d974c65a4
이정훈 <eu4ng97@gmail.com>
2024-05-07 01:00:53 +0900

    feat: GASExtension 모듈 추가

---
+++ Source/GASExtension/Private/GASExtension.cpp
@@ -0,0 +1,17 @@
+﻿#include "GASExtension.h"
+
+#define LOCTEXT_NAMESPACE "FGASExtensionModule"
+
+void FGASExtensionModule::StartupModule()
+{
+    
+}
+
+void FGASExtensionModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGASExtensionModule, GASExtension)
