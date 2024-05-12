188ff20c085a6eda5e58cf560808efc8ddbf3895
이정훈 <eu4ng97@gmail.com>
2024-05-12 18:23:28 +0900

    feat: TPS 전용 캐릭터 클래스 작성

---
+++ Source/GASGameFramework/Public/Character/GGFTPSCharacter.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GGFCharacter.h"
+#include "GGFTPSCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+
+/**
+ * TPS 전용 캐릭터 클래스
+ */
+UCLASS()
+class GASGAMEFRAMEWORK_API AGGFTPSCharacter : public AGGFCharacter
+{
+    GENERATED_BODY()
+
+public:
+    /* CameraBoom의 이름으로 다른 클래스로 교체할 때 사용합니다. */
+    static FName CameraBoomName;
+
+    /* FollowCamera의 이름으로 다른 클래스로 교체할 때 사용합니다. */
+    static FName FollowCameraName;
+
+    /** Camera boom positioning the camera behind the character */
+    UPROPERTY(VisibleAnywhere, BlueprintGetter = GetCameraBoom, Category = "Component")
+    TObjectPtr<USpringArmComponent> CameraBoom;
+
+    /** Follow camera */
+    UPROPERTY(VisibleAnywhere, BlueprintGetter = GetFollowCamera, Category = "Component")
+    TObjectPtr<UCameraComponent> FollowCamera;
+
+public:
+    AGGFTPSCharacter();
+
+public:
+    /* Getter */
+
+    UFUNCTION(BlueprintGetter)
+    FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+    UFUNCTION(BlueprintGetter)
+    FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
