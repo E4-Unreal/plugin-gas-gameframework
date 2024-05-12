38ef695e54ee304daae842792a438fceab86408e
이정훈 <eu4ng97@gmail.com>
2024-05-12 20:24:54 +0900

    feat: TPSSample 전용 데미지 계산식 클래스 작성
    데미지: Source 공격력 - Target 방어력
    데미지는 방어막(Shield)를 우선 감소시킨 후 체력을 감소시킵니다.

---
+++ Source/GGFTPSSample/Public/GGFTPSDamageCalculation.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "GGFTPSDamageCalculation.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GGFTPSSAMPLE_API UGGFTPSDamageCalculation : public UGameplayEffectExecutionCalculation
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
