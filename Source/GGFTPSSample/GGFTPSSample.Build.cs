4619b875e60102843bd514defeea74916a25b04c
이정훈 <eu4ng97@gmail.com>
2024-05-12 18:34:56 +0900

    chore: GGF TPSSample 모듈 추가

---
+++ Source/GGFTPSSample/GGFTPSSample.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class GGFTPSSample : ModuleRules
+{
+    public GGFTPSSample(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/GGFTPSSample/Private/GGFTPSSample.cpp
@@ -0,0 +1,17 @@
+﻿#include "GGFTPSSample.h"
+
+#define LOCTEXT_NAMESPACE "FGGFTPSSampleModule"
+
+void FGGFTPSSampleModule::StartupModule()
+{
+    
+}
+
+void FGGFTPSSampleModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGGFTPSSampleModule, GGFTPSSample)
