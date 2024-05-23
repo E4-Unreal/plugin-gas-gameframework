59a62fc0b8b1c7dab685bb667189540c3127ce60
이정훈 <eu4ng97@gmail.com>
2024-05-23 15:45:49 +0900

    chore: SteamExtension 모듈 추가

---
+++ Source/SteamExtension/Private/SteamExtension.cpp
@@ -0,0 +1,17 @@
+﻿#include "SteamExtension.h"
+
+#define LOCTEXT_NAMESPACE "FSteamExtensionModule"
+
+void FSteamExtensionModule::StartupModule()
+{
+    
+}
+
+void FSteamExtensionModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSteamExtensionModule, SteamExtension)
---
+++ Source/SteamExtension/SteamExtension.Build.cs
@@ -0,0 +1,33 @@
+﻿using UnrealBuildTool;
+
+public class SteamExtension : ModuleRules
+{
+    public SteamExtension(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+
+                // UI
+                "UMG",
+
+                // 온라인 서브 시스템 스팀
+                "OnlineSubsystem",
+                "OnlineSubsystemSteam"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
