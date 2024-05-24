aa1fdeee3c484c207aa4355f87ed6fb74777569a
이정훈 <eu4ng97@gmail.com>
2024-05-24 14:25:53 +0900

    chore: GGFCharacterSystem 모듈 추가

---
+++ Source/GGFCharacterSystem/Private/GGFCharacterSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "GGFCharacterSystem.h"
+
+#define LOCTEXT_NAMESPACE "FGGFCharacterSystemModule"
+
+void FGGFCharacterSystemModule::StartupModule()
+{
+    
+}
+
+void FGGFCharacterSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGGFCharacterSystemModule, GGFCharacterSystem)
