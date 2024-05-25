4473dc1e2fcf90489131bfeaf2082ca61044e491
이정훈 <eu4ng97@gmail.com>
2024-05-25 11:13:55 +0900

    chore: GGFDataSystem 모듈 추가

---
+++ Source/GGFDataSystem/Private/GGFDataSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "GGFDataSystem.h"
+
+#define LOCTEXT_NAMESPACE "FGGFDataSystemModule"
+
+void FGGFDataSystemModule::StartupModule()
+{
+    
+}
+
+void FGGFDataSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGGFDataSystemModule, GGFDataSystem)
