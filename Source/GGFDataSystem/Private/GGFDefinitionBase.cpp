bc0cb9dd9b447bb5eef88db4c55bf82ef04a97e3
이정훈 <eu4ng97@gmail.com>
2024-05-25 11:36:00 +0900

    feat: 데이터 시스템에서 사용되는 기본 데이터 에셋 클래스 작성

---
+++ Source/GGFDataSystem/Private/GGFDefinitionBase.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GGFDefinitionBase.h"
+
+void UGGFDefinitionBase::InitFromDataTable(UDataTable* NewDataTable, int32 NewID)
+{
+    // 입력 유효성 검사
+    if(NewDataTable == nullptr || NewID < 0) return;
+
+    // 데이터 설정
+    DataTable = NewDataTable;
+    ID = NewID;
+
+    // 초기화
+    Internal_InitFromDataTable();
+}
+
+void UGGFDefinitionBase::Internal_InitFromDataTable_Implementation()
+{
+    // 데이터 테이블 구조체를 이용하여 데이터 에셋 초기화 진행
+}
---
+++ Source/GGFDataSystem/Public/GGFDefinitionBase.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GGFDefinitionBase.generated.h"
+
+/**
+ * 데이터 시스템에서 사용되는 기본 데이터 에셋 클래스로 데이터 테이블로부터 초기화를 위한 가상 메서드가 정의되어 있습니다.
+ */
+UCLASS(BlueprintType, Blueprintable)
+class GGFDATASYSTEM_API UGGFDefinitionBase : public UDataAsset
+{
+    GENERATED_BODY()
+
+    UPROPERTY(VisibleAnywhere, Category = "State", Transient)
+    TObjectPtr<UDataTable> DataTable;
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = 0))
+    int32 ID;
+
+public:
+    /* API */
+
+    UFUNCTION(BlueprintCallable)
+    void InitFromDataTable(UDataTable* NewDataTable, int32 NewID);
+
+protected:
+    /* 메서드 */
+
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void Internal_InitFromDataTable();
+};
