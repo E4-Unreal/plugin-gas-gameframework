57fcd5e1fcd70e5c5d479a5a1c95d57a1764d91f
이정훈 <eu4ng97@gmail.com>
2024-05-25 11:43:21 +0900

    feat: GGFDataSystem 전용 프로젝트 설정 기능 추가

--- Source/GGFDataSystem/Private/GGFDataSystem.cpp
+++ Source/GGFDataSystem/Private/GGFDataSystem.cpp
@@ -1,17 +1,28 @@
 ﻿#include "GGFDataSystem.h"
 
+#include "GGFDataSystemSetting.h"
+#include "Developer/Settings/Public/ISettingsModule.h"
+
+IMPLEMENT_MODULE(FGGFDataSystemModule, GGFDataSystem)
+
 #define LOCTEXT_NAMESPACE "FGGFDataSystemModule"
 
 void FGGFDataSystemModule::StartupModule()
 {
-    
+    if(ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+    {
+        SettingsModule->RegisterSettings("Project", "Plugins", "GGFDataSystem",
+            LOCTEXT("RuntimeSettingsName", "GGF DataSystem"), LOCTEXT("RuntimeSettingsDescription", "GGF DataSystem"),
+            GetMutableDefault<UGGFDataSystemSetting>());
+    }
 }
 
 void FGGFDataSystemModule::ShutdownModule()
 {
-    
+    if(ISettingsModule* SettingsModule = FModuleManager::GetModulePtr<ISettingsModule>("Settings"))
+    {
+        SettingsModule->UnregisterSettings("Project", "Plugins", "GGFDataSystem");
+    }
 }
 
 #undef LOCTEXT_NAMESPACE
-    
-IMPLEMENT_MODULE(FGGFDataSystemModule, GGFDataSystem)
---
+++ Source/GGFDataSystem/Public/GGFDataSystemSetting.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "GGFDataSystemSetting.generated.h"
+
+class UGGFDefinitionBase;
+
+/**
+ * GGFDataSystem에서 사용할 프로젝트 설정 전용 데이터 클래스
+ */
+UCLASS(Config = GASGameFramework)
+class GGFDATASYSTEM_API UGGFDataSystemSetting : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(Config, EditAnywhere, Category = "Config")
+    TMap<TSubclassOf<UGGFDefinitionBase>, TSoftObjectPtr<UDataTable>> DefinitionDataTableMap;
+};
