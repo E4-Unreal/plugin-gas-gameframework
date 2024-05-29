4bf2a2c14b180111461b2ec810418d818bfbc370
이정훈 <eu4ng97@gmail.com>
2024-05-29 20:55:43 +0900

    chore: GGFCombatCore 모듈 추가

---
+++ Source/GGFCombatCore/GGFCombatCore.Build.cs
@@ -0,0 +1,34 @@
+﻿using UnrealBuildTool;
+
+public class GGFCombatCore : ModuleRules
+{
+    public GGFCombatCore(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+
+                // GAS
+                "GameplayTags",
+                "GameplayTasks",
+                "GameplayAbilities",
+
+                // GASGameFramework
+                "GGFDataSystem"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/GGFCombatCore/Private/GGFCombatCore.cpp
@@ -0,0 +1,17 @@
+﻿#include "GGFCombatCore.h"
+
+#define LOCTEXT_NAMESPACE "FGGFCombatCoreModule"
+
+void FGGFCombatCoreModule::StartupModule()
+{
+    
+}
+
+void FGGFCombatCoreModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FGGFCombatCoreModule, GGFCombatCore)
