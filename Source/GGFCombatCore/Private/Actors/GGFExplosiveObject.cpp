d9c6559f3b72eb1933261c652f1014fab4e0106d
이정훈 <eu4ng97@gmail.com>
2024-06-04 19:04:56 +0900

    feat: 데미지를 입으면 폭발하는 오브젝트 클래스 작성

---
+++ Source/GGFCombatCore/Private/Actors/GGFExplosiveObject.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/GGFExplosiveObject.h"
+
+#include "Components/GGFExplosiveComponent.h"
+#include "Components/SphereComponent.h"
+
+AGGFExplosiveObject::AGGFExplosiveObject()
+{
+    // 리플리케이트 설정
+    SetReplicates(true);
+
+    // ExplosionArea
+    ExplosionArea = CreateDefaultSubobject<USphereComponent>(TEXT("ExplosionSphere"));
+    ExplosionArea->SetupAttachment(RootComponent);
+    ExplosionArea->SetSphereRadius(200);
+
+    // ExplosiveComponent
+    ExplosiveComponent = CreateDefaultSubobject<UGGFExplosiveComponent>(TEXT("ExplosiveComponent"));
+    ExplosiveComponent->Init(ExplosionArea);
+}
+
+void AGGFExplosiveObject::Destroyed()
+{
+    ExplosiveComponent->Explode();
+
+    Super::Destroyed();
+}
+
+void AGGFExplosiveObject::OnDead_Implementation()
+{
+    Destroy();
+}
