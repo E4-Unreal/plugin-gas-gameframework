5d6ad87135504235962dd2e4226ceb912c2531e0
이정훈 <eu4ng97@gmail.com>
2024-06-07 18:19:47 +0900

    feat: Pawn, Character 전용 어빌리티 클래스 작성

---
+++ Source/GASExtension/Private/Abilities/GEPawnAbility.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Abilities/GEPawnAbility.h"
+
+void UGEPawnAbility::OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
+{
+    Super::OnAvatarSet(ActorInfo, Spec);
+
+    AvatarPawn = CastChecked<APawn>(ActorInfo->AvatarActor);
+}
---
+++ Source/GASExtension/Public/Abilities/GEPawnAbility.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GEGameplayAbility.h"
+#include "GEPawnAbility.generated.h"
+
+/**
+ * Pawn 전용 어빌리티 클래스
+ */
+UCLASS()
+class GASEXTENSION_API UGEPawnAbility : public UGEGameplayAbility
+{
+    GENERATED_BODY()
+
+    TWeakObjectPtr<APawn> AvatarPawn;
+
+public:
+    /* GameplayAbility */
+
+    virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+protected:
+    UFUNCTION(BlueprintPure)
+    FORCEINLINE APawn* GetAvatarPawn() const { return AvatarPawn.Get(); }
+};
