9c667a45022146b756fc9faf27a9766b8c0b58ac
이정훈 <eu4ng97@gmail.com>
2024-06-09 04:43:21 +0900

    chore: 기믹 트리거 연동 전용 컴포넌트 작성

---
+++ Source/GGFGimmickSystem/Private/Components/GGFTriggerComponent.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/GGFTriggerComponent.h"
+
+#include "Interfaces/GGFActivationInterface.h"
+#include "Logging.h"
+
+void UGGFTriggerComponent::ActivateTargets()
+{
+    for (auto Target : Targets)
+    {
+        if(!Target)
+        {
+#if WITH_EDITOR
+            LOG_ACTOR_COMPONENT_DETAIL(Error, TEXT("Targets에 null 값이 포함되어 있습니다."))
+#endif
+            continue;
+        }
+
+        if(Target->Implements<UGGFActivationInterface>())
+        {
+            IGGFActivationInterface::Execute_Activate(Target, GetOwner());
+#if WITH_EDITOR
+            LOG_ACTOR_COMPONENT_DETAIL(Log, TEXT("Try Activate %s"), *Target->GetName())
+#endif
+        }
+    }
+}
+
+void UGGFTriggerComponent::DeactivateTargets()
+{
+    for (auto Target : Targets)
+    {
+        if(Target->Implements<UGGFActivationInterface>())
+        {
+            IGGFActivationInterface::Execute_Deactivate(Target, GetOwner());
+#if WITH_EDITOR
+            LOG_ACTOR_COMPONENT_DETAIL(Log, TEXT("Try Deactivate %s"), *Target->GetName())
+#endif
+        }
+    }
+}
---
+++ Source/GGFGimmickSystem/Public/Components/GGFTriggerComponent.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/GGFActorComponent.h"
+#include "GGFTriggerComponent.generated.h"
+
+/**
+ * 연동된 액터들을 활성화 혹은 비활성화하는 액터 컴포넌트입니다.
+ * 주로 버튼 같은 기믹에서 사용됩니다.
+ */
+UCLASS(meta=(BlueprintSpawnableComponent))
+class GGFGIMMICKSYSTEM_API UGGFTriggerComponent : public UGGFActorComponent
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Reference")
+    TArray<TObjectPtr<AActor>> Targets;
+
+public:
+    /* API */
+
+    UFUNCTION(BlueprintCallable)
+    virtual void ActivateTargets();
+
+    UFUNCTION(BlueprintCallable)
+    virtual void DeactivateTargets();
+
+protected:
+    /* GGFActorComponent */
+
+    virtual FORCEINLINE bool IsValid() const override { return !Targets.IsEmpty(); }
+};
