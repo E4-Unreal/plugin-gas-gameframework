9aee592902b726057f38eec8de3d2548ef02b2fc
이정훈 <eu4ng97@gmail.com>
2024-06-09 00:51:37 +0900

    feat: 타임라인 기능이 포함된 기본 액터 클래스 작성

---
+++ Source/GGFGimmickSystem/Private/Actors/GGFTimelineActor.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/GGFTimelineActor.h"
+
+#include "Components/TimelineComponent.h"
+
+AGGFTimelineActor::AGGFTimelineActor()
+{
+    Timeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timeline"));
+    TimelineInterpFunction.BindDynamic(this, &ThisClass::OnTimelineActivated);
+    TimelineFinishedFunction.BindDynamic(this, &ThisClass::OnTimelineFinished);
+
+    const ConstructorHelpers::FObjectFinder<UCurveFloat> TimelineCurveFinder(TEXT("/Niagara/DefaultAssets/Curves/Templates/LinearRampUp.LinearRampUp"));
+    if(TimelineCurveFinder.Succeeded()) TimelineCurveFloat = TimelineCurveFinder.Object;
+}
+
+void AGGFTimelineActor::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    Timeline->AddInterpFloat(TimelineCurveFloat, TimelineInterpFunction);
+    Timeline->SetTimelineFinishedFunc(TimelineFinishedFunction);
+}
+
+void AGGFTimelineActor::PlayTimeline(bool bReverse, bool bReset)
+{
+    auto CachedTimeline = GetTimeline();
+
+    if(bUseCustomDuration)
+    {
+        auto PlayRate = CachedTimeline->GetTimelineLength() / Duration;
+        CachedTimeline->SetPlayRate(PlayRate);
+    }
+
+    if(bReverse)
+    {
+        if(bReset) CachedTimeline->ReverseFromEnd();
+        else CachedTimeline->Reverse();
+    }
+    else
+    {
+        if(bReset) CachedTimeline->PlayFromStart();
+        else CachedTimeline->Play();
+    }
+}
+
+void AGGFTimelineActor::OnTimelineActivated_Implementation(float Value)
+{
+
+}
+
+void AGGFTimelineActor::OnTimelineFinished_Implementation()
+{
+}
