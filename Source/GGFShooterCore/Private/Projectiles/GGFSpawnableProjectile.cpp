ea738d7c1329a7d43509b5ba7b96a3a70bcfc125
이정훈 <eu4ng97@gmail.com>
2024-06-08 16:23:32 +0900

    feat: 충돌 위치에 특정 액터를 스폰하는 발사체 클래스 작성

---
+++ Source/GGFShooterCore/Private/Projectiles/GGFSpawnableProjectile.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Projectiles/GGFSpawnableProjectile.h"
+
+void AGGFSpawnableProjectile::OnSphereColliderHit_Implementation(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+    // 스폰
+    if(HasAuthority())
+    {
+        FVector SpawnLocation;
+        FRotator SpawnRotation;
+        FActorSpawnParameters ActorSpawnParameters;
+        ActorSpawnParameters.Instigator = GetInstigator();
+        ActorSpawnParameters.Owner = GetOwner();
+
+        if(bUseHitResult)
+        {
+            SpawnLocation = Hit.ImpactPoint;
+            SpawnRotation = Hit.ImpactNormal.ToOrientationRotator();
+        }
+        else
+        {
+            SpawnLocation = GetActorLocation();
+            SpawnRotation = GetActorRotation();
+        }
+
+        GetWorld()->SpawnActor(ActorToSpawn, &SpawnLocation, &SpawnRotation, ActorSpawnParameters);
+    }
+
+    Super::OnSphereColliderHit_Implementation(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit);
+}
