6b1d9662e85e18340e592f91fb7ef0860df58f84
이정훈 <eu4ng97@gmail.com>
2024-06-09 13:59:07 +0900

    feat: 점프 패드 클래스 작성

---
+++ Source/GGFGimmickSystem/Private/Actors/GGFJumpPad.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/GGFJumpPad.h"
+
+#include "Components/ArrowComponent.h"
+#include "GameFramework/Character.h"
+
+AGGFJumpPad::AGGFJumpPad()
+{
+    /* LaunchDirectionComponent */
+    LaunchDirectionComponent = CreateDefaultSubobject<UArrowComponent>(TEXT("LaunchDirectionComponent"));
+    LaunchDirectionComponent->SetupAttachment(RootComponent);
+    LaunchDirectionComponent->SetRelativeRotation(FRotator(90, 0, 0));
+
+    /* 기본 설정 */
+    ActorClassToLaunch = AActor::StaticClass();
+}
+
+void AGGFJumpPad::OnTriggerBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    Super::OnTriggerBoxBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep,
+                                    SweepResult);
+
+    if(OtherActor->GetClass()->IsChildOf(ActorClassToLaunch))
+    {
+        Launch(OtherActor);
+    }
+}
+
+void AGGFJumpPad::Launch_Implementation(AActor* Target)
+{
+    FVector LaunchVelocity = GetLaunchDirectionComponent()->GetRelativeRotation().Vector() * LaunchPower;
+
+    if(auto Character = Cast<ACharacter>(Target))
+    {
+        Character->LaunchCharacter(LaunchVelocity, true, true);
+    }
+    else if(auto PrimitiveComponent = Cast<UPrimitiveComponent>(Target->GetComponentByClass(UPrimitiveComponent::StaticClass())))
+    {
+        PrimitiveComponent->SetPhysicsLinearVelocity(LaunchVelocity);
+    }
+}
