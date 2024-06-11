c8976c145c4ae313f481cbd3b623260d38d40262
이정훈 <eu4ng97@gmail.com>
2024-06-11 16:53:32 +0900

    feat: 피직스 기본 액터 클래스 작성

---
+++ Source/GGFCore/Public/Actors/GGFPhysicsActor.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "GGFPhysicsActor.generated.h"
+
+/**
+ * 멀티플레이 피직스 시뮬레이트 전용 스태틱 메시 액터입니다.
+ */
+UCLASS()
+class GGFCORE_API AGGFPhysicsActor : public AStaticMeshActor
+{
+    GENERATED_BODY()
+
+public:
+    AGGFPhysicsActor();
+};
