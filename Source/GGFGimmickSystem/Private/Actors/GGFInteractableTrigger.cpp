b4107d9b44d73cefa17fe00142ca2c735e79bb9b
이정훈 <eu4ng97@gmail.com>
2024-06-12 01:29:45 +0900

    feat: InteractableTrigger 클래스 작성

---
+++ Source/GGFGimmickSystem/Private/Actors/GGFInteractableTrigger.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/GGFInteractableTrigger.h"
+
+#include "Components/GGFActivatableComponent.h"
+#include "Components/GGFTriggerComponent.h"
+
+AGGFInteractableTrigger::AGGFInteractableTrigger()
+{
+    /* Trigger Component */
+    TriggerComponent = CreateDefaultSubobject<UGGFTriggerComponent>(TEXT("TriggerComponent"));
+
+    /* Activatable Component */
+    ActivatableComponent = CreateDefaultSubobject<UGGFActivatableComponent>(TEXT("ActivatableComponent"));
+    ActivatableComponent->SetupAttachment(GetDisplayMesh());
+}
+
+void AGGFInteractableTrigger::OnPawnInteract_Implementation(APawn* OtherPawn)
+{
+    GetTriggerComponent()->ToggleActivateTargets();
+}
+
+bool AGGFInteractableTrigger::Activate_Implementation(AActor* InstigatorActor)
+{
+    return GetActivatableComponent()->TryActivate(this);
+}
+
+bool AGGFInteractableTrigger::Deactivate_Implementation(AActor* InstigatorActor)
+{
+    return GetActivatableComponent()->TryDeactivate(this);
+}
