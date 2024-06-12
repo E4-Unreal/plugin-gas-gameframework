3e3ddd8b4bdbda7b31bec1bbd947ed979649c383
이정훈 <eu4ng97@gmail.com>
2024-06-12 18:55:54 +0900

    feat: 상호작용 가능한 아이템 클래스 작성

---
+++ Source/GGFItemSystem/Private/Actors/GGFInteractableItem.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/GGFInteractableItem.h"
+
+#include "Components/GGFInteractableComponent.h"
+#include "Components/GGFItemDataManager.h"
+#include "Data/GGFItemDefinition.h"
+
+AGGFInteractableItem::AGGFInteractableItem()
+{
+    /* Item Manager */
+    ItemDataManager = CreateDefaultSubobject<UGGFItemDataManager>(TEXT("ItemDataManager"));
+}
+
+void AGGFInteractableItem::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    /* Item Manager */
+    ItemDataManager->OnIDUpdated.AddDynamic(this, &ThisClass::OnIDUpdated);
+}
+
+void AGGFInteractableItem::OnConstruction(const FTransform& Transform)
+{
+    Super::OnConstruction(Transform);
+
+    // 초기화
+    OnIDUpdated(ItemDataManager->GetID());
+}
+
+void AGGFInteractableItem::OnIDUpdated(int32 NewID)
+{
+    // 아이템 데이터 설정
+    const auto& ItemData = GetItemDataManager()->GetData();
+    GetDisplayMesh()->SetStaticMesh(ItemData.DisplayMesh);
+    GetInteractableComponent()->DisplayName = ItemData.DisplayName;
+}
---
+++ Source/GGFItemSystem/Public/Actors/GGFInteractableItem.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/GGFInteractableActor.h"
+#include "GGFInteractableItem.generated.h"
+
+class UGGFItemDataManager;
+
+/**
+ * 레벨에 존재하는 상호작용 가능한 아이템입니다.
+ */
+UCLASS()
+class GGFITEMSYSTEM_API AGGFInteractableItem : public AGGFInteractableActor
+{
+    GENERATED_BODY()
+
+    // 아이템 데이터 서브 시스템을 사용하기 위한 전용 컴포넌트
+    UPROPERTY(VisibleAnywhere, BlueprintGetter = GetItemDataManager, Category = "Component")
+    TObjectPtr<UGGFItemDataManager> ItemDataManager;
+
+public:
+    AGGFInteractableItem();
+
+    /* AActor */
+
+    virtual void PostInitializeComponents() override;
+    virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+    /* 이벤트 */
+
+    UFUNCTION(BlueprintCallable)
+    virtual void OnIDUpdated(int32 NewID);
+
+public:
+    /* Getter */
+
+    UFUNCTION(BlueprintGetter)
+    FORCEINLINE UGGFItemDataManager* GetItemDataManager() const { return ItemDataManager; }
+};
