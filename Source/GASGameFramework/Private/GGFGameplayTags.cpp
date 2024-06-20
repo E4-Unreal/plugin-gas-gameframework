7f8efc076416118b73881db7ed09714559b53a81
이정훈 <eu4ng97@gmail.com>
2024-06-20 17:54:24 +0900

    chore: 스킬 전용 입력 액션 추가 및 트리거 설정 변경

--- Source/GASGameFramework/Private/GGFGameplayTags.cpp
+++ Source/GASGameFramework/Private/GGFGameplayTags.cpp
@@ -2,5 +2,15 @@
 
 namespace GGFGameplayTags
 {
-
+    /* 플레이어 입력 */
+    namespace Input
+    {
+        namespace Ability
+        {
+            UE_DEFINE_GAMEPLAY_TAG_COMMENT(Root, "Input.Ability", "스킬 전용 입력 태그")
+            UE_DEFINE_GAMEPLAY_TAG_COMMENT(Test, "Input.Ability.Test", "테스트 전용 스킬 입력 태그")
+            UE_DEFINE_GAMEPLAY_TAG_COMMENT(Q, "Input.Ability.Q", "Q 스킬 입력 태그")
+            UE_DEFINE_GAMEPLAY_TAG_COMMENT(E, "Input.Ability.E", "E 스킬 입력 태그")
+        }
+    }
 }
