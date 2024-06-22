802e059dcc1edc4faf2c9c3171b7467e1684ae8d
이정훈 <eu4ng97@gmail.com>
2024-06-23 03:05:01 +0900

    feat: 자가 버프 전용 어빌리티 클래스 작성

---
+++ Source/GGFSkillSystem/Private/Abilities/GGFGA_ApplyBuffToSelf.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Abilities/GGFGA_ApplyBuffToSelf.h"
+
+#include "AbilitySystemComponent.h"
+
+void UGGFGA_ApplyBuffToSelf::PreActivate(const FGameplayAbilitySpecHandle Handle,
+                                         const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                         FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate, const FGameplayEventData* TriggerEventData)
+{
+    Super::PreActivate(Handle, ActorInfo, ActivationInfo, OnGameplayAbilityEndedDelegate, TriggerEventData);
+
+    // 초기화
+    bApplied = false;
+}
+
+void UGGFGA_ApplyBuffToSelf::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+    const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+    const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    if(bApplyImmediately) ApplyBuffToSelf();
+}
+
+void UGGFGA_ApplyBuffToSelf::OnPlayMontageNotifyBegin_Implementation(FName NotifyName)
+{
+    Super::OnPlayMontageNotifyBegin_Implementation(NotifyName);
+
+    ApplyBuffToSelf();
+}
+
+void UGGFGA_ApplyBuffToSelf::OnMontageCompleted_Implementation()
+{
+    Super::OnMontageCompleted_Implementation();
+
+    ApplyBuffToSelf();
+}
+
+bool UGGFGA_ApplyBuffToSelf::InternalCanActivate()
+{
+    return Super::InternalCanActivate() && BuffClass;
+}
+
+void UGGFGA_ApplyBuffToSelf::ApplyBuffToSelf()
+{
+    if(bApplied) return;
+    bApplied = true;
+
+    auto AbilitySystem = GetAbilitySystemComponentFromActorInfo();
+    AbilitySystem->ApplyGameplayEffectToSelf(BuffClass->GetDefaultObject<UGameplayEffect>(), GetAbilityLevel(), AbilitySystem->MakeEffectContext());
+}
