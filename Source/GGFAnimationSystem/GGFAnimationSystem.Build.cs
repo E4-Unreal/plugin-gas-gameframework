4fcd6ed578408661ac7efcf9ef2c2459552c3f3b
이정훈 <eu4ng97@gmail.com>
2024-06-23 14:22:50 +0900

    chore: GGFAnimationSystem 모듈 추가

---
+++ Source/GGFAnimationSystem/GGFAnimationSystem.Build.cs
@@ -0,0 +1,34 @@
+﻿using UnrealBuildTool;
+
+public class GGFAnimationSystem : ModuleRules
+{
+    public GGFAnimationSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+
+                // GAS
+                "GameplayTags",
+                "GameplayTasks",
+                "GameplayAbilities",
+
+                // GASGameFramework
+                "GGFCore"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
