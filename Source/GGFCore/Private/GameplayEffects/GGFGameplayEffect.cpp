09272d392df9da5829954905d994fb2f7c143df8
이정훈 <eu4ng97@gmail.com>
2024-06-25 04:32:26 +0900

    refactor: GGFGameplayEffect 모듈 이전 및 컴포넌트 추가

---
+++ Source/GGFCore/Private/GameplayEffects/GGFGameplayEffect.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameplayEffects/GGFGameplayEffect.h"
+
+#include "GameplayEffectComponents/TargetTagRequirementsGameplayEffectComponent.h"
+#include "GGFGameplayTags.h"
+#include "GameplayEffectComponents/TargetTagsGameplayEffectComponent.h"
+
+UGGFGameplayEffect::UGGFGameplayEffect()
+{
+    /* TargetTagRequirementsComponent */
+    TargetTagRequirementsComponent = CreateDefaultSubobject<UTargetTagRequirementsGameplayEffectComponent>(TEXT("TargetTagRequirementsComponent"));
+    TargetTagRequirementsComponent->ApplicationTagRequirements.IgnoreTags.AddTagFast(State::Dead);
+    TargetTagRequirementsComponent->OngoingTagRequirements.IgnoreTags.AddTagFast(State::Dead);
+    TargetTagRequirementsComponent->RemovalTagRequirements.RequireTags.AddTagFast(State::Dead);
+
+    /* TargetTagsGameplayEffectComponent */
+    TargetTagsGameplayEffectComponent = CreateDefaultSubobject<UTargetTagsGameplayEffectComponent>(TEXT("TargetTagsGameplayEffectComponent"));
+
+    /* 컴포넌트 등록 */
+    GEComponents.Emplace(TargetTagRequirementsComponent.Get());
+    GEComponents.Emplace(TargetTagsGameplayEffectComponent.Get());
+}
+
+UTargetTagRequirementsGameplayEffectComponent* UGGFGameplayEffect::
+GetOrFindTargetTagRequirementsComponent()
+{
+    return GetOrFindComponent(TargetTagRequirementsComponent).Get();
+}
+
+UTargetTagsGameplayEffectComponent* UGGFGameplayEffect::GetOrFindTargetTagsGameplayEffectComponent()
+{
+    return GetOrFindComponent(TargetTagsGameplayEffectComponent).Get();
+}
+
+template <class T>
+TWeakObjectPtr<T> UGGFGameplayEffect::GetOrFindComponent(TWeakObjectPtr<T>& Component) const
+{
+    if(!Component.IsValid())
+    {
+        for (auto GEComponent : GEComponents)
+        {
+            if(GEComponent->GetClass()->IsChildOf(T::StaticClass()))
+            {
+                Component = CastChecked<T>(GEComponent);
+            }
+        }
+    }
+
+    return Component;
+}
---
+++ Source/GGFCore/Public/GameplayEffects/GGFGameplayEffect.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GGFGameplayEffect.generated.h"
+
+class UTargetTagsGameplayEffectComponent;
+class UTargetTagRequirementsGameplayEffectComponent;
+
+/**
+ * GASGameFramework 플러그인 전용 게임플레이 이펙트 클래스
+ */
+UCLASS()
+class GGFCORE_API UGGFGameplayEffect : public UGameplayEffect
+{
+    GENERATED_BODY()
+
+    /* 컴포넌트 */
+
+    TWeakObjectPtr<UTargetTagRequirementsGameplayEffectComponent> TargetTagRequirementsComponent;
+    TWeakObjectPtr<UTargetTagsGameplayEffectComponent> TargetTagsGameplayEffectComponent;
+
+public:
+    UGGFGameplayEffect();
+
+    /* API */
+
+    UFUNCTION(BlueprintCallable)
+    UTargetTagRequirementsGameplayEffectComponent* GetOrFindTargetTagRequirementsComponent();
+
+    UFUNCTION(BlueprintCallable)
+    UTargetTagsGameplayEffectComponent* GetOrFindTargetTagsGameplayEffectComponent();
+
+protected:
+    template<class T>
+    TWeakObjectPtr<T> GetOrFindComponent(TWeakObjectPtr<T>& Component) const;
+};
