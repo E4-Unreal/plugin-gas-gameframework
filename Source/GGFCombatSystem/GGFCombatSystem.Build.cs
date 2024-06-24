d165e553263ee4873be17189809b2fcfa9ccbaba
이정훈 <eu4ng97@gmail.com>
2024-06-24 12:44:57 +0900

    chore : GGFCombatSystem 로그 설정

--- Source/GGFCombatSystem/GGFCombatSystem.Build.cs
+++ Source/GGFCombatSystem/GGFCombatSystem.Build.cs
@@ -17,6 +17,7 @@ public class GGFCombatSystem : ModuleRules
                 "GameplayAbilities",
 
                 // GASGameFramework
+                "GGFCore",
                 "GASExtension",
                 "GGFDataSystem"
             }
