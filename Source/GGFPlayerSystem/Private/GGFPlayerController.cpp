4f1479483d4f0cbef5ec1c88f9a35ac7d047dafb
이정훈 <eu4ng97@gmail.com>
2024-06-25 20:20:26 +0900

    feat: GGFTeamInterface가 구현된 GGFPlayerController 클래스 작성

---
+++ Source/GGFPlayerSystem/Private/GGFPlayerController.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GGFPlayerController.h"
+
+#include "GameFramework/PlayerState.h"
+
+uint8 AGGFPlayerController::GetTeamID_Implementation() const
+{
+    if(PlayerState->Implements<UGGFTeamInterface>())
+    {
+        return Execute_GetTeamID(PlayerState);
+    }
+
+    return NoTeam;
+}
+
+void AGGFPlayerController::SetTeamID_Implementation(uint8 NewTeamID)
+{
+    if(PlayerState->Implements<UGGFTeamInterface>())
+    {
+        return Execute_SetTeamID(PlayerState, NewTeamID);
+    }
+}
---
+++ Source/GGFPlayerSystem/Public/GGFPlayerController.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Interfaces/GGFTeamInterface.h"
+#include "GGFPlayerController.generated.h"
+
+/**
+ * GASGameFramework 플러그인 전용 플레이어 컨트롤러
+ */
+UCLASS()
+class GGFPLAYERSYSTEM_API AGGFPlayerController : public APlayerController, public IGGFTeamInterface
+{
+    GENERATED_BODY()
+
+public:
+    /* GGFTeamInterface */
+
+    virtual uint8 GetTeamID_Implementation() const override;
+    virtual void SetTeamID_Implementation(uint8 NewTeamID) override;
+};
