44d1dbfce1e2ed5a8d971409edb800b384ba4464
이정훈 <eu4ng97@gmail.com>
2024-06-25 18:15:22 +0900

    chore: GGFPlayerSystem 모듈 추가

---
+++ Source/GGFPlayerSystem/GGFPlayerSystem.Build.cs
@@ -0,0 +1,29 @@
+﻿using UnrealBuildTool;
+
+public class GGFPlayerSystem : ModuleRules
+{
+    public GGFPlayerSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+
+                // GASGameFramework
+                "GGFCore"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
