3ceceb886b089eb97b1508e1d2b8c4cbfc0c4b14
이정훈 <eu4ng97@gmail.com>
2024-06-29 13:23:40 +0900

    chore: WidgetSystem 모듈 추가

---
+++ Source/GGFWidgetSystem/GGFWidgetSystem.Build.cs
@@ -0,0 +1,37 @@
+﻿using UnrealBuildTool;
+
+public class GGFWidgetSystem : ModuleRules
+{
+    public GGFWidgetSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+
+                // UI
+                "UMG",
+
+                // GAS
+                "GameplayTags",
+                "GameplayTasks",
+                "GameplayAbilities",
+
+                // GASGameFramework
+                "GGFCore"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
