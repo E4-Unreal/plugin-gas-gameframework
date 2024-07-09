f40b889f1df1f8dbb983c609015f607531aa71c3
이정훈 <eu4ng97@gmail.com>
2024-07-09 22:32:58 +0900

    fix: 하위 위젯 자동 주입 코드 제거

--- Source/GGFWidgetSystem/Private/Widgets/GGFUserWidget.cpp
+++ Source/GGFWidgetSystem/Private/Widgets/GGFUserWidget.cpp
@@ -18,15 +18,6 @@ void UGGFUserWidget::SetTargetActor(AActor* NewTargetActor)
 {
     TargetActor = NewTargetActor;
     SetTargetSystem(UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(TargetActor.Get()));
-
-    // 하위 위젯에도 Target 주입
-    TArray<UWidget*> ChildWidgets;
-    WidgetTree->GetAllWidgets(ChildWidgets);
-
-    for (auto ChildWidget : ChildWidgets)
-    {
-        if(auto CastedWidget = Cast<UGGFUserWidget>(ChildWidget)) CastedWidget->SetTargetActor(NewTargetActor);
-    }
 }
 
 void UGGFUserWidget::SetTargetSystem(UAbilitySystemComponent* NewTargetSystem)
