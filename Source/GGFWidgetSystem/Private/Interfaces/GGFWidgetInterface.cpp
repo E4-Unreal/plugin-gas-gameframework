ff1f1efdeba10bf92846ca713b7b0acd52717a7c
이정훈 <eu4ng97@gmail.com>
2024-07-09 19:42:27 +0900

    feat: 위젯 전용 인터페이스 클래스 작성

---
+++ Source/GGFWidgetSystem/Private/Interfaces/GGFWidgetInterface.cpp
@@ -0,0 +1,5 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Interfaces/GGFWidgetInterface.h"
+
+// Add default functionality here for any IGGFWidgetInterface functions that are not pure virtual.
---
+++ Source/GGFWidgetSystem/Public/Interfaces/GGFWidgetInterface.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GGFWidgetInterface.generated.h"
+
+UINTERFACE(Blueprintable)
+class GGFWIDGETSYSTEM_API UGGFWidgetInterface : public UInterface
+{
+    GENERATED_BODY()
+};
+
+/**
+ * 위젯 전용 인터페이스
+ */
+class GGFWIDGETSYSTEM_API IGGFWidgetInterface
+{
+    GENERATED_BODY()
+
+public:
+    // 위젯 갱신
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+    void Refresh();
+};
